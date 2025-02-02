a44dc80e4b104bdd5ce46b93413870921981f78d
MLSBHARGAVASAI <mlsbhargavasai5002@gmail.com>
2025-02-02 13:55:58 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let username = document.getElementById("username").value;
         let password = document.getElementById("password").value;
 
-        if (username === "Sahith" && password === "Sahith") {
+        if (username === "TEACHER" && password === "SEP-5") {
             window.location.href = "admin.html";
         } else {
             alert("Invalid username or password");
